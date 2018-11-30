04a993eb4ab4d512816d04ed6c1ce2ccbed7664a
ThatSpartan <dodo4931@outlook.com>
2018-11-30 12:29:48 -0500

    done choix multiples

--- script_forChoixMultiples.js
+++ script_forChoixMultiples.js
@@ -10,23 +10,37 @@ function play_animation(g) {
     }
 }
 
-let i = 0;
-let question_element = document.getElementById("question");
+let i = 0, choix_element;
+let question_element = document.getElementById("question"), choix_container = document.getElementById("choix_container");
 
-question_element.innerHTML = "questions[i].question";
-console.log(questions);
-
-function f(sel) {
-
-}
-
-function new_question() {
-    // if () {
-    //     play_animation(true);
-    // } else {
-    //     play_animation(false);
-    // }
-
-    i++;
+function show_options() {
     question_element.innerHTML = questions[i].question;
+
+    while (choix_container.firstChild) {
+        choix_container.removeChild(choix_container.firstChild);
+    }
+    for (let index = 0; index < questions[i].options.length; index++) {
+        const option = questions[i].options[index];
+        choix_element = document.createElement("p");
+        choix_element.addEventListener("click", call_from_button);
+        choix_element.innerHTML = option;
+
+        if (index == questions[i].good_option_index) {
+            choix_element.id = "goodone";
+        }
+
+        choix_container.appendChild(choix_element);
+    }
+}
+show_options();
+
+
+function call_from_button() {
+    if (this.id == "goodone") {
+        play_animation(true);
+    } else {
+        play_animation(false);
+    }
+    i++;
+    show_options();
 }
